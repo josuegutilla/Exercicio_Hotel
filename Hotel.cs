5eeebe4079b4560cfafb5aa69514504f207fe4c9
Josué <gutillaj@gmail.com>
2023-01-25 00:19:05 -0400

    Projeto Hotel criado

---
+++ Hotel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Hotel_Arrays
+{
+    public class Hotel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public Hotel(string nome, string email)
+        {
+            Name = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Name 
+                + ", " 
+                + Email;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Projeto_Hotel_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hotel hotel;
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int quant = int.Parse(Console.ReadLine());
+
+            Hotel[] vetor = new Hotel[10];
+
+            for(int i = 1; i <= quant; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Aluguel #" + i + ":");
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                vetor[quarto] = new Hotel(nome, email);
+            }
+
+            Console.WriteLine("Quartos ocupados: ");
+            for(int i = 0; i < 10; i++)
+            {
+                if(vetor[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vetor[i]);
+                }
+            }
+        }
+    }
+}
